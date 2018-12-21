badf7a02875d810f0ef2b2497f296eaa614f6812
Christina L. Cole <christinalcole@gmail.com>
2018-12-20 23:38:04 -0500

    add specs for comment list

--- src/containers/CommentFeed.test.js
+++ src/containers/CommentFeed.test.js
@@ -3,11 +3,36 @@ import { render } from 'react-testing-library'
 import CommentFeed from './CommentFeed'
 
 describe(CommentFeed, () => {
-  const props = { header: "Comment Feed", comments: [] }
+  let props = { header: "Comment Feed", comments: [] }
 
   it("renders the CommentFeed", () => {
     const { queryByText } = render(<CommentFeed {...props} />)
     const header = queryByText(props.header)
     expect(header.innerHTML).toBe(props.header)
+  })
+
+  it("renders the comment list", () => {
+    const { container } = render(<CommentFeed {...props} />)
+    const commentNodes = container.querySelectorAll('.Comment')
+    expect(commentNodes.length).toBe(props.comments.length)
+  })
+
+  it("renders the comment list with some entries", () => {
+    let comments = [
+      {
+        author: "Wilson",
+        text: "Get me off this island",
+      },
+      {
+        author: "Max Headroom",
+        text: "Wh-wh-what'd you say again"
+      }
+    ]
+
+    props = { header: "Comment Feed", comments }
+    
+    const { container } = render(<CommentFeed {...props} />)
+    const commentNodes = container.querySelectorAll('.Comment')
+    expect(commentNodes.length).toBe(props.comments.length)
   })
 })
