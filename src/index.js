06cf49cf8b645de11e9ff9a4de0276ca6320af1b
Christina L. Cole <christinalcole@gmail.com>
2019-01-02 21:20:22 -0500

    UI mockup to confirm visuals

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import CommentFeed from './containers/CommentFeed';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const comments = [
+  {
+    author: "Wilson",
+    text: "When I get off this island, I'm going to Disney",
+  },
+  {
+    author: "Sting",
+    text: "Woke up this morning, can't believe what I saw",
+  },
+  {
+    author: "Peter Garrett",
+    text: "I hear the bullroarer"
+  },
+]
+
+ReactDOM.render(
+  <CommentFeed comments = { comments } />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
