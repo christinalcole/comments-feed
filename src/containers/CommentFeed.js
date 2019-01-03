291a57edae645d682fe727d7fbd185b349186fe5
Christina L. Cole <christinalcole@gmail.com>
2019-01-02 22:41:49 -0500

    WIP: add comment form, tests -- not passing :neutral_face:

--- src/containers/CommentFeed.js
+++ src/containers/CommentFeed.js
@@ -2,6 +2,22 @@ import React, { Component } from 'react'
 import Comment from '../containers/Comment'
 
 export default class CommentFeed extends Component {
+  state = {
+    author: "",
+    text: "",
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+
+    const { author, text } = this.state
+    this.props.createComment({ author, text })
+  }
+
+  handleChange = event => {
+    this.setState({ [event.target.id]: event.target.value })
+  }
+  
   renderComments() {
     return this.props.comments.map((comment, i) => (
       <Comment key={i} {...comment} />
@@ -13,6 +29,32 @@ export default class CommentFeed extends Component {
     return (
       <div className="CommentFeed">
         <h2>{header}</h2>
+
+        <form
+          className="comment-form"
+          onSubmit={this.handleSubmit}
+        >
+          <label htmlFor="author">
+            Author
+            <input
+              id="author"
+              type="text"
+              onChange={this.handleChange}
+            />
+          </label>
+
+          <label htmlFor="text">
+            Comment
+            <input
+              id="text"
+              type="text"
+              onChange={this.handleChange}
+            />
+          </label>
+
+          <button type="submit">Submit Comment</button>
+        </form>
+
         <div className="comment-list">
           {this.renderComments()}
         </div>
--- src/containers/CommentFeed.test.js
+++ src/containers/CommentFeed.test.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react'
-import { render } from 'react-testing-library'
+import { render, Simulate } from 'react-testing-library'
 import CommentFeed from './CommentFeed'
+
+const createProps = props => ({
+  header: 'Comment Feed',
+  comments: [
+    {
+      author: "Wilson",
+      text: "Get me off this island",
+    },
+    {
+      author: "Max Headroom",
+      text: "Wh-wh-what'd you say again"
+    },
+  ],
+  createComment: jest.fn(),
+  ...props,
+})
 
 describe(CommentFeed, () => {
   let props = { header: "Comment Feed", comments: [] }
@@ -30,9 +46,28 @@ describe(CommentFeed, () => {
     ]
 
     props = { header: "Comment Feed", comments }
-    
+
     const { container } = render(<CommentFeed {...props} />)
     const commentNodes = container.querySelectorAll('.Comment')
     expect(commentNodes.length).toBe(props.comments.length)
+  })
+
+  it("allows the user to add a comment", () => {
+    const newComment = { author: "Claudius", comment: "Who spiked this punch?" }
+    let props = createProps()
+    const { container, getByLabelText } = render(<CommentFeed {...props} />)
+    const authorNode = getByLabelText("Author")
+    const textNode = getByLabelText("Comment")
+    const formNode = container.querySelector("form")
+
+    authorNode.value = newComment.author
+    textNode.value = newComment.text
+
+    Simulate.change(authorNode)
+    Simulate.change(textNode)
+    Simulate.submit(formNode)
+
+    expect(props.createComment).toHaveBeenCalledTimes(1)
+    expect(props.createComment).toHaveBeenCalledWith(newComment)
   })
 })
